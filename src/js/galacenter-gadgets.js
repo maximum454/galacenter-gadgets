f48687004c9928e3a9bdab459e4f9f335e390cb4
Maxim Karachyov <karachyov.m@gmail.com>
2022-03-16 21:20:14 +0300

    slider

--- src/js/galacenter-gadgets.js
+++ src/js/galacenter-gadgets.js
@@ -1,3 +1,13 @@
-$(function (){
+const sliderGadgetsSwiper = new Swiper('.slider-gadgets-swiper', {
+    loop: true,
+    slidesPerView: 1,
+    centeredSlides: true,
+    spaceBetween: 30,
 
-})
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
